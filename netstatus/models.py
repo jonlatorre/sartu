9d0a7903f447fd887c0b56a4786a2bc7d506dc52
Jon Latorre <moebius@etxea.net>
2013-11-16 11:04:21 +0100

    Version que funciona lo básico

--- netstatus/models.py
+++ netstatus/models.py
@@ -8,19 +8,19 @@ class NetStatus():
 		self.internet = 0
 		self.prey = 0
 		self.vpn = 0
-		self.isartu1 = 0
-		self.isartu2 = 0
-		self.isartu3 = 0
+		self.ibsartu1 = 0
+		self.ibsartu2 = 0
+		self.ibsartu3 = 0
 		print "Vamos a comprobar el estatus"
 		print "Primero probamos internet..."
 		nm = nmap.PortScanner()
-		nm.scan('google.com','80')
+		nm.scan('google.com','80','-T5')
 		if nm[ nm.all_hosts()[0]].state() == "up":
 			print "Hay conexion a inet"
 			self.internet = 1
 		
 		print "Ahora sartu prey..."
-		nm.scan('alava.sartu.org','80')
+		nm.scan('alava.sartu.org','80','-T5')
 		if nm[ nm.all_hosts()[0]].state() == "up":
 			print "Hay conexion a prey"
 			self.prey = 1
@@ -33,11 +33,15 @@ class NetStatus():
 				self.vpn = 1
 		
 		print "Ahora probamos cada maquina..."
-		host = "192.168.176.10"
+		host = "192.168.176.10-12"
 		port = "3389"
-		nm.scan(host,port)
-		if len(nm.all_hosts()) != 0:
-			if nm[ nm.all_hosts()[0]].tcp(port)["state"]=="open":
-				self.ibsartu1 = 1
-		else:
-			print "No hay conexion a las maquinas"
+		nm.scan(host,port,'-T5')
+		if nm.has_host("192.168.176.10"):
+			if nm["192.168.176.10"].tcp(int(port))["state"]=="open":
+				self.ibsartu1=1
+		if nm.has_host("192.168.176.11"):
+			if nm["192.168.176.11"].tcp(int(port))["state"]=="open":
+				self.ibsartu2=1
+		if nm.has_host("192.168.176.12"):
+			if nm["192.168.176.12"].tcp(int(port))["state"]=="open":
+				self.ibsartu3=1
